f59b984d8e14a54d36950ec4789b495f9da2940c
Витёк Ромашов <romas-viktor@yandex.ru>
2024-03-23 09:19:57 +0300

    Добавьте файлы проекта.

---
+++ DZ_31.02.2024_Crawler/Program.cs
@@ -0,0 +1,119 @@
+﻿//Написать класс CrawlerPathLogger, который выполняет логирование директорий и файлов этих директорий на заданном диске.
+
+using static System.Console;
+
+public class CrawlerPathLogger
+{
+    private string[] DirsArr;
+    private string FilePathLog;
+    private string LogicDiskLetter;
+
+    public string[] GetDirsArr()
+    {
+        return DirsArr;
+    }
+
+    public void SetDirsArr(string[] dirs)
+    {
+        DirsArr = dirs;
+    }
+
+    public string GetFilePathLog()
+    {
+        return FilePathLog;
+    }
+
+    public void SetFilePathLog(string filePath)
+    {
+        FilePathLog = filePath;
+    }
+
+    public string GetLogicDiskLetter()
+    {
+        return LogicDiskLetter;
+    }
+
+    public void SetLogicDiskLetter(string diskLetter)
+    {
+        LogicDiskLetter = diskLetter;
+    }
+
+    public void RecursiveDirectoryTraversal(string path)
+    {
+        try
+        {
+            DirsArr = Directory.GetDirectories(path, "*", SearchOption.AllDirectories);
+        }
+        catch (UnauthorizedAccessException ex)
+        {
+            WriteLine($"Ошибка доступа к каталогу: {ex.Message}");
+        }
+    }
+
+    public void WriteToLogFile()
+    {
+        using (StreamWriter sw = File.CreateText(FilePathLog))
+        {
+            foreach (string dir in DirsArr)
+            {
+                sw.WriteLine($"{DateTime.Now}, {dir}");
+
+                string[] files = Directory.GetFiles(dir);
+                foreach (string file in files)
+                {
+                    FileInfo fileInfo = new FileInfo(file);
+                    sw.WriteLine($"    -> {fileInfo.CreationTime}, {fileInfo.Name}, {fileInfo.Extension}");
+                }
+
+                string[] subdirs = Directory.GetDirectories(dir);
+                foreach (string subdir in subdirs)
+                {
+                    sw.WriteLine($"    -> {DateTime.Now}, {subdir}");
+
+                    string[] subFiles = Directory.GetFiles(subdir);
+                    foreach (string subFile in subFiles)
+                    {
+                        FileInfo subFileInfo = new FileInfo(subFile);
+                        sw.WriteLine($"        -> {subFileInfo.CreationTime}, {subFileInfo.Name}, {subFileInfo.Extension}");
+                    }
+                }
+            }
+        }
+    }
+
+    public void ReadFromFile()
+    {
+        try
+        {
+            using (StreamReader sr = new StreamReader(FilePathLog))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    WriteLine(line);
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            WriteLine($"Произошла ошибка при чтении файла: {ex.Message}");
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        CrawlerPathLogger crawler = new CrawlerPathLogger();
+        crawler.SetDirsArr(new string[] { "dir1", "dir2" });
+        crawler.SetFilePathLog("logFile.txt");
+        crawler.SetLogicDiskLetter("C://");
+
+        string directoryPath = "C:\\AMD"; 
+
+        crawler.RecursiveDirectoryTraversal(directoryPath);
+        crawler.WriteToLogFile();
+        crawler.ReadFromFile();
+    }
+}
